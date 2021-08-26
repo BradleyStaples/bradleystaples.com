73d2ddbe9ec5844b88570b6386029d61fffb6f95
Bradley Staples <the.bradley.staples@gmail.com>
2021-08-26 15:59:55 -0700

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
     title: 'Bradley Staples',
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-30225574-1',
+        head: true
+      }
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-gatsby-cloud',
     'gatsby-plugin-react-helmet'
