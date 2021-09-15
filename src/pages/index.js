2acc8d3d24bc343db4bc0c7f93c235771c14d69f
Bradley Staples <the.bradley.staples@gmail.com>
2021-09-15 14:09:28 -0700

    link to projects page. update favicon path. hide randle/fontbeast projects.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
+
 import Page from '../components/page';
+
 
 const IndexPage = () => {
   return (
     <Page title='Bradley Staples' h2='A Proven Software Engineer'>
       <p>Bradley has over a decade of web development and software engineering experience, primarily in front-end development building professional custom user interfaces ranging from complex web apps to marketing websites.</p>
+      <p>To see a few of his projects, see the <Link to='/projects'>projects</Link> page.</p>
+
     </Page>
   )
 }
